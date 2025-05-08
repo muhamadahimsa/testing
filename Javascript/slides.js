cc3ceb322e5c4c3f64c5872bf08194934f2bc1ca
muhamadahimsa <ahimson2@gmail.com>
2025-05-08 08:24:02 +0700

    new test

---
+++ Javascript/slides.js
@@ -0,0 +1,22 @@
+export const slides = [
+  {
+    tag: "Website",
+    marquee: "Eclipse Interactive Art Portfolio",
+    image: "./Asset/Images/img1.jpg",
+  },
+  {
+    tag: "Branding",
+    marquee: "Eclipse Interactive Art Portfolio",
+    image: "./Asset/Images/img2.jpg",
+  },
+  {
+    tag: "Marketing",
+    marquee: "Eclipse Interactive Art Portfolio",
+    image: "./Asset/Images/img3.jpg",
+  },
+  {
+    tag: "Website",
+    marquee: "Eclipse Interactive Art Portfolio",
+    image: "./Asset/Images/img4.jpg",
+  },
+];
